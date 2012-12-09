26bfee61b7cebbdd992d3c8d0b1ae468c17c5a83
U-Tuo-PC\Tuo <Tuo@Tuo-PC.(none)>
2012-12-09 17:14:57 +0800

    add windows support

--- app.js
+++ app.js
@@ -11,15 +11,7 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
-
-var sys = require('sys');
-var exec = require('child_process').exec;
-var child;
-var serial = "/dev/cu.SLAB_USBtoUART"; //make sure the serial port is connnected
-var setupCommand = "/bin/stty -f " + serial + " cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts";
-
-
-
+ 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -40,15 +32,7 @@ app.get('/', routes.index);
 app.post('/control', control.control);
 app.get('/users', user.list);
 
-child = exec(setupCommand, function(error, stdout, stderr){
-	sys.print('stdout: ' + stdout);
-	sys.print('stderr: ' + stderr);		
-	if(error !== null){
-		console.log("exec command: "  + setupCommand + " failed with error: "+ error);
-	}else{
-		console.log('exec command: ' + setupCommand + " worked")	
-	}
-});
+ 
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
